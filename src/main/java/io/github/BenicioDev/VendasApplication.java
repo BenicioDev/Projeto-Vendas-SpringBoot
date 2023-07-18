957c140ef74a549a944a0cd59b5d3f13452f3e09
Gabriel <gabrielvitor083@gmail.com>
2023-07-18 01:24:07 -0300

    update

--- src/main/java/io/github/BenicioDev/VendasApplication.java
+++ src/main/java/io/github/BenicioDev/VendasApplication.java
@@ -1,13 +1,42 @@
 package io.github.BenicioDev;
 
+import io.github.BenicioDev.domain.entity.Cliente;
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.BenicioDev.domain.repositorio.Clientes;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    String Message = "teste";
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+          Cliente cliente = new Cliente();
+          cliente.setNome("Benicio");
+          clientes.salvar(cliente);
+
+            Cliente cliente1 = new Cliente();
+            cliente.setNome("Gabriel");
+            clientes.salvar(cliente);
+
+            List <Cliente> todosClientes = clientes.obterLista();
+            todosClientes.forEach(System.out::println);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
+
     }
 }
--- src/main/java/io/github/BenicioDev/domain/entity/Cliente.java
+++ src/main/java/io/github/BenicioDev/domain/entity/Cliente.java
@@ -3,6 +3,14 @@ package io.github.BenicioDev.domain.entity;
 public class Cliente {
 
     private Integer id;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
     private String nome;
 
     public Integer getId() {
@@ -17,7 +25,20 @@ public class Cliente {
         return nome;
     }
 
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
     }
 }
